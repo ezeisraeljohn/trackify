10fe85bc128701a347f29d4be338e364f0445cd8
Israel Eze <ezeisraeljohn@gmail.com>
2025-05-24 20:46:31 +0100

    fix: Include UserReturnDetails and Token in schema exports

--- app/schemas/__init__.py
+++ app/schemas/__init__.py
@@ -1,3 +1,3 @@
-from .user import UserCreate, UserUpdate
+from .user import UserCreate, UserUpdate, UserReturnDetails, Token
 
-__all__ = ["UserCreate", "UserUpdate"]
+__all__ = ["UserCreate", "UserUpdate", "UserReturnDetails", "Token"]
