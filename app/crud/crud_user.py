110373f6d577504e985038c28cf0c625d755dc2b
Israel Eze <ezeisraeljohn@gmail.com>
2025-05-24 20:43:13 +0100

    refactor: Rename create_user to insert_user and improve password hashing

--- app/crud/crud_user.py
+++ app/crud/crud_user.py
@@ -1,15 +1,19 @@
-from fastapi import HTTPException, status
 from sqlmodel import Session
 from app.models import User
-from app.schemas import UserUpdate, UserCreate
+from app.schemas import UserCreate
+from sqlmodel import select
+from app.api.deps import password_hash
 
 
-def create_user(db: Session, user: UserCreate) -> User:
+def insert_user(db: Session, user: UserCreate) -> User:
     """
     Create a new user in the database.
     """
+    hashed_password = password_hash(user.hashed_password)
+    user.hashed_password = hashed_password
     db_user = User.model_validate(user)
     db.add(db_user)
     db.commit()
     db.refresh(db_user)
-    return db_user
+    return_message = db_user
+    return return_message
