5f6237b1d548df0b92ea5b6e9196f55a2090efd6
Israel Eze <ezeisraeljohn@gmail.com>
2025-05-24 20:41:39 +0100

    feat: Implement user authentication and JWT token generation with error handling

---
+++ app/api/deps.py
@@ -0,0 +1,98 @@
+from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
+from fastapi import Depends, HTTPException, status
+from sqlmodel.orm.session import Session
+from app.models import User
+from sqlmodel import select
+from app.db.session import get_session
+from passlib.context import CryptContext
+from dotenv import load_dotenv
+import os
+import jwt
+from datetime import datetime, timedelta, timezone
+
+load_dotenv()
+
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="login")
+
+
+def verify_password(plain_password: str, hashed_password: str) -> bool:
+    """
+    Verify a plain password against a hashed password.
+    """
+    return pwd_context.verify(plain_password, hashed_password)
+
+
+def password_hash(password: str) -> str:
+    """
+    Hash a password.
+    """
+    return pwd_context.hash(password)
+
+
+def get_user_by_email(db: Session, email: str) -> User | None:
+    """
+    Get a user by email.
+    """
+    user = db.exec(select(User).where(User.email == email)).first()
+    return user
+
+
+def authenticate_user(db: Session, email: str, password: str) -> User | None:
+    """
+    Authenticate a user by email and password.
+    """
+    user = get_user_by_email(db=db, email=email)
+    if not user or not verify_password(password, user.hashed_password):
+        return None
+    return user
+
+
+def create_access_token(data: dict, expires_delta: timedelta | None) -> str:
+    """
+    Create a JWT access token.
+    """
+    to_encode = data.copy()
+    if expires_delta:
+        expire = datetime.now(timezone.utc) + expires_delta
+    else:
+        expire = datetime.now(timezone.utc) + timedelta(
+            minutes=float(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES", 30.00))
+        )
+    secret_key = os.getenv("SECRET_KEY")
+    algorithm = os.getenv("ALGORITHM")
+    if not secret_key or not algorithm:
+        raise RuntimeError("SECRET_KEY and ALGORITHM environment variables must be set")
+    to_encode.update({"exp": expire})
+    encoded_jwt = jwt.encode(to_encode, secret_key, algorithm=algorithm)
+    return encoded_jwt
+
+
+def get_current_user(
+    token: str = Depends(oauth2_scheme), db: Session = Depends(get_session)
+) -> User:
+    """
+    Get the current user from the token.
+    """
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Could not validate credentials",
+        headers={"WWW-Authenticate": "Bearer"},
+    )
+    try:
+        secret_key = os.getenv("SECRET_KEY")
+        algorithm = os.getenv("ALGORITHM")
+        if not secret_key or not algorithm:
+            raise RuntimeError(
+                "SECRET_KEY and ALGORITHM environment variables must be set"
+            )
+        payload = jwt.decode(token, secret_key, algorithms=[algorithm])
+        email: str = payload.get("sub")
+        if email is None:
+            raise credentials_exception
+    except jwt.PyJWTError:
+        raise credentials_exception
+    user = get_user_by_email(db=db, email=email)
+    if user is None:
+        raise credentials_exception
+    return user
