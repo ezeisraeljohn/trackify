ce6b94c09a9258004179cdb651918190754abf59
Israel Eze <ezeisraeljohn@gmail.com>
2025-05-26 20:20:28 +0100

    feat: Update __all__ exports to include InsightGenerateReturnList, LLMResponse, and LLMQuery

--- app/schemas/__init__.py
+++ app/schemas/__init__.py
@@ -1,3 +1,13 @@
 from .user import UserCreate, UserUpdate, UserReturnDetails, Token
+from .insight import InsightGenerateReturnList
+from .assistant import LLMResponse, LLMQuery
 
-__all__ = ["UserCreate", "UserUpdate", "UserReturnDetails", "Token"]
+__all__ = [
+    "UserCreate",
+    "UserUpdate",
+    "UserReturnDetails",
+    "Token",
+    "InsightGenerateReturnList",
+    "LLMResponse",
+    "LLMQuery",
+]
