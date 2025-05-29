ea7fd35bbfbcdfe84d8c9829421f4f3f54a1ea0f
Israel Eze <ezeisraeljohn@gmail.com>
2025-05-29 20:59:09 +0100

    feat: remove unused imports from auth.py

--- app/api/v1/endpoints/auth.py
+++ app/api/v1/endpoints/auth.py
@@ -1,10 +1,7 @@
 from fastapi import APIRouter, Depends, HTTPException
 from sqlmodel.orm.session import Session
-from app.api.deps import get_user_by_email
-from app.crud import insert_user
-from app.schemas import UserCreate, UserReturnDetails, Token
+from app.schemas import Token
 from app.db.session import get_session
-from app.models import User
 from fastapi.security import OAuth2PasswordRequestForm
 from ...deps import authenticate_user, create_access_token
 from datetime import timedelta
