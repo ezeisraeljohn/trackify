021108506be4e4cd3b2c92f6b04a0937193a78f0
Israel Eze <ezeisraeljohn@gmail.com>
2025-05-31 22:32:38 +0100

    feat: implement user registration endpoint with email validation

--- app/api/v1/endpoints/auth.py
+++ app/api/v1/endpoints/auth.py
@@ -7,6 +7,12 @@ from ...deps import authenticate_user, create_access_token
 from datetime import timedelta
 import os
 from dotenv import load_dotenv
+from app.schemas import UserCreate, UserReturnDetails
+from fastapi.security import OAuth2PasswordRequestForm
+from ...deps import authenticate_user, create_access_token
+from datetime import timedelta
+from app.api.deps import get_user_by_email
+from app.crud import insert_user
 
 load_dotenv()
 
@@ -38,6 +44,26 @@ def login(
             token_type="bearer",
             expires_in=int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES", "30"))
             * 60,  # Convert to seconds
+        )
+    except Exception as e:
+        raise HTTPException(status_code=400, detail=str(e))
+
+
+@router.post("/register", response_model=UserReturnDetails, status_code=201)
+def create_user(
+    user: UserCreate, db: Session = Depends(get_session)
+) -> UserReturnDetails | None:
+    """
+    Create a new user.
+    """
+
+    try:
+        db_user = get_user_by_email(db=db, email=user.email)
+        if db_user:
+            raise HTTPException(status_code=400, detail="Email already registered")
+        value = insert_user(db=db, user=user)
+        return UserReturnDetails(
+            status="success", message="User created successfully", data=value
         )
     except Exception as e:
         raise HTTPException(status_code=400, detail=str(e))
