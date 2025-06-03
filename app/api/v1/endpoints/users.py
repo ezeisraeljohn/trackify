79c2560badf5065d222b5de46d4a5bb3b059748e
Israel Eze <ezeisraeljohn@gmail.com>
2025-06-03 22:36:12 +0100

    refactor: remove dotenv dependency and streamline user creation logic

--- app/api/v1/endpoints/users.py
+++ app/api/v1/endpoints/users.py
@@ -9,29 +9,7 @@ from fastapi.security import OAuth2PasswordRequestForm
 from ...deps import authenticate_user, create_access_token
 from datetime import timedelta
 import os
-from dotenv import load_dotenv
-
-load_dotenv()
+from app.core import settings
 
 
 router = APIRouter(prefix="/api/v1/users", tags=["Users"])
-
-
-@router.post("/", response_model=UserReturnDetails, status_code=201)
-def create_user(
-    user: UserCreate, db: Session = Depends(get_session)
-) -> UserReturnDetails | None:
-    """
-    Create a new user.
-    """
-
-    try:
-        db_user = get_user_by_email(db=db, email=user.email)
-        if db_user:
-            raise HTTPException(status_code=400, detail="Email already registered")
-        value = insert_user(db=db, user=user)
-        return UserReturnDetails(
-            status="success", message="User created successfully", data=value
-        )
-    except Exception as e:
-        raise HTTPException(status_code=400, detail=str(e))
