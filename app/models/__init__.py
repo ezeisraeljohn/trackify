6e0a6b84d7c55ba651becfa4be8737730804bf04
Israel Eze <ezeisraeljohn@gmail.com>
2025-06-03 22:43:30 +0100

    feat: include OTP model in __all__ for module exports

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -2,5 +2,6 @@ from .account import LinkedAccount
 from .transaction import Transaction
 from .user import User
 from .insight import Insight
+from .otp import OTP
 
-__all__ = ["User", "LinkedAccount", "Transaction", "Insight"]
+__all__ = ["User", "LinkedAccount", "Transaction", "Insight", "OTP"]
