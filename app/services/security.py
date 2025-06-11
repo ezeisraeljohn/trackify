aca0e5a1c45b8dfc6d501dc30b2f7fe745200e6f
Israel Eze <ezeisraeljohn@gmail.com>
2025-06-11 10:42:46 +0100

    feat: implement SecurityService for data encryption and decryption

---
+++ app/services/security.py
@@ -0,0 +1,28 @@
+from cryptography.fernet import Fernet
+from app.utils.logger import logger
+from app.core import settings
+from typing import Union
+
+
+class SecurityService:
+    def __init__(self):
+        if not settings.ENCRYPTION_KEY:
+            raise ValueError("ENCRYPTION_KEY not set in settings")
+        self.cipher = Fernet(settings.ENCRYPTION_KEY.encode())
+
+    def encrypt(self, data: Union[str, int, float]) -> str:
+        """
+        Encrypt the given data using the secret key.
+        """
+        if not data:
+            raise ValueError("Data to encrypt cannot be empty")
+        return self.cipher.encrypt(str(data).encode()).decode()
+
+    def decrypt(self, encrypted_data: str) -> str:
+        """
+        Decrypt the given encrypted data using the secret key.
+        """
+        if not encrypted_data:
+            logger.error("encrypted cannot be empty", exc_info=True)
+            raise ValueError("Encrypted data cannot be empty")
+        return self.cipher.decrypt(encrypted_data.encode()).decode()
